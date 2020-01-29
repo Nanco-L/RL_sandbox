77188cc11b671936054ba1705152590834a93cd6
Kyuhyun Lee <khlee1992@snu.ac.kr>
2020-01-29 16:20:56 +0900

    Update agent.py

--- agent/agent.py
+++ agent/agent.py
@@ -4,11 +4,15 @@ import tensorflow as tf
 class Agent():
     def __init__(self):
         self.result_log = list()
+        self.player_number = 0
 
     def get_move(self):
         """
         Override
         """
+
+    def set_player_number(self, number):
+        self.player_number = number
 
     def save_result(self, result, playlog):
         self.result_log.append([playlog, result])
@@ -39,29 +43,51 @@ class DQNBot(Agent):
         super().__init__()
         self.model = model
         self.replay = list()
-        self.player_number = 0
         self.decay_rate = 0.9
-
-    def set_player_number(self, number):
-        self.player_number = number
+        self.exploration_rate = 1.
 
     def get_move(self, state, avail_action):
-        values = self.model.model(state)
-        action = np.max(values[avail_action])
+        if np.random.random() < self.exploration_rate:
+            action_list = np.arange(avail_action.shape[0])[avail_action]
+            np.random.shuffle(action_list)
+    
+            return action_list[0]
+        else:
+            values = self.model.model(state)
         
-        return np.max(values[avail_action])
+            return np.argmax(values[avail_action])
 
     def save_result(self, result, playlog):
 
-        s_ = None
+        s_ = np.ones(playlog[-1][-1].shape)
         while playlog:
             i = len(playlog)
+            s, a, _ = playlog.pop()
 
-            if i%2 == self.player_number:
-                s, a, _ = playlog.pop()
+            if self.player_number == 1:
+                s = ((s+2)*2)%3
+
+            if (i+1)%2 == self.player_number:
                 self.replay.append([s, a, result, s_])
                 s_ = s
 
             result *= self.decay_rate
+
+    def generate_dataset(self):
+        s = list()
+        a = list()
+        r = list()
+        s_ = list()
+
+        for item in self.replay:
+            s.append(item[0])
+            a.append(item[1])
+            r.append(item[2])
+            s_.append(item[3])
+
+        self.train_ds = tf.data.Dataset.from_tensor_slices((s,a,r,s_)).shuffle(2000).batch(16)
+
+    def train(self):
+        self.model.run()
 
         
