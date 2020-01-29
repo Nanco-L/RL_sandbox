a0579be78bf2e00e0e9f969d9acb61c11dcff294
Kyuhyun Lee <khlee1992@snu.ac.kr>
2020-01-29 16:14:48 +0900

    Update model.py

--- agent/model.py
+++ agent/model.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 class FCN(tf.keras.Model):
     def __init__(self, io, hidden):
         super(FCN, self).__init__()
+        
         self.d1 = tf.keras.layers.Dense(hidden, activation='sigmoid')
         self.d2 = tf.keras.layers.Dense(hidden, activation='sigmoid')
         self.d3 = tf.keras.layers.Dense(io, activation='linear')
@@ -25,15 +26,29 @@ class CNN(tf.keras.Model):
 class DQNWrapper():
     def __init__(self, model):
         self.model = model
+        self.loss_object = tf.keras.losses.MeanSquaredError()
+        self.optimizer = tf.keras.optimizers.Adam()
+        self.train_loss = tf.keras.metrics.Mean(name='train_loss')
     
     @tf.function
-    def train_step(self, x, y):
+    def train_step(self, s, a, r, s_):
         with tf.GradientTape() as tape:
-            predictions = self.model(x)
+            # TODO: 
+            predictions = self.model(s)
+            
             #print(y)
             #print(predictions)
             loss = self.loss_object(y, predictions)
             
         gradients = tape.gradient(loss, self.model.trainable_variables)
         self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
-        self.loss_mean(loss)
+        self.train_loss(loss)
+
+    def run(self, epochs, train_ds):
+        for epoch in range(epochs):
+            for s, a, r, s_ in train_ds:
+                self.train_step(s, a, r, s_)
+
+            if epoch % 10:
+                print(f'{self.train_loss.result()}')
+
