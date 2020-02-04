d9393ad69b58a40aeacc8d894f8f0599eb168cc8
Kyuhyun Lee <khlee1992@snu.ac.kr>
2020-02-04 18:09:12 +0900

    Update test_board.py

--- test_board.py
+++ test_board.py
@@ -1,15 +1,25 @@
+import tensorflow as tf
+import numpy as np
+import agent.model as am
 import board.board as bd
 import agent.agent as ag
+
+fc = am.FCN(9,10)
+dqn = am.DQNWrapper(fc)
+#dqn.load()
 
 myboard = bd.Tictactoe()
 H1 = ag.Human()
 H2 = ag.Human()
-B1 = ag.DQNBot(None)
-B2 = ag.DQNBot(None)
+B1 = ag.DQNBot(dqn)
+B2 = ag.DQNBot(dqn)
 
-for i in range(3):
-    myboard.run(B1, B2, verbose=False)
-    print(len(B1.replay))
+B1.exploration_rate = 0.
+B2.exploration_rate = 0.
+
+for i in range(1):
+    myboard.run(B1, B2, verbose=True)
+    print(B1.replay)
 
 #print(B1.replay)
 #print(B2.replay)
