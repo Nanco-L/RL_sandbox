b0f3f9b439dd9278623d2da795b9693bbdbc8ba9
Kyuhyun Lee <khlee1992@snu.ac.kr>
2020-02-06 17:20:29 +0900

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -4,7 +4,7 @@ import agent.model as am
 import board.board as bd
 import agent.agent as ag
 
-fc = am.FCN(9,10)
+fc = am.FCN(9,30)
 dqn = am.DQNWrapper(fc)
 
 myboard = bd.Tictactoe()
@@ -27,14 +27,17 @@ for ii in range(1000):
     tset1 = B1.generate_dataset()
     tset2 = B2.generate_dataset()
 
-    global_epoch = dqn.run(50, tset1.concatenate(tset2).shuffle(10000).batch(16), global_epoch)
+    #global_epoch = dqn.run(50, tset1.concatenate(tset2).shuffle(10000).batch(16), global_epoch)
+    global_epoch = dqn.run(5, tset1.shuffle(10000).batch(16), global_epoch)
 
-    if (ii+1)%10 == 0:
-        #if B1.exploration_rate > 0.25:
-        #    B1.exploration_rate -= 0.15
-        #if B2.exploration_rate > 0.25: 
-        #    B2.exploration_rate -= 0.15
+    if (ii+1)%50 == 0:
+        if B1.exploration_rate > 0.1:
+            B1.exploration_rate -= 0.2
+            print(f'exploration_rate: {B1.exploration_rate}')
+        if B2.exploration_rate > 0.1: 
+            B2.exploration_rate -= 0.2
         print(f'{dqn.model(np.array([[1.,-1.,-1.,1.,1.,-1.,-1.,1.,0.]]))}, {np.sum(dqn.model(np.array([[1.,-1.,-1.,1.,1.,-1.,-1.,1.,0.]])).numpy())}')
+        print(f'{dqn.model(np.array([[0.,-1.,-1.,0.,0.,1.,0.,0.,1.]]))}, {np.sum(dqn.model(np.array([[0.,-1.,-1.,0.,0.,1.,0.,0.,1.]])).numpy())}')
         dqn.save()
 
     #B1.clear_replay()
